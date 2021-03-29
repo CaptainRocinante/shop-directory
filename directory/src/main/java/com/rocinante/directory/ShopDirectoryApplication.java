2118d86b2475b69da623dba2e2ab5b8985a22d65
Captain Rocinante <58204640+CaptainRocinante@users.noreply.github.com>
2021-03-29 13:06:10 -0700

    Big refactor, Spring setup

---
+++ directory/src/main/java/com/rocinante/directory/ShopDirectoryApplication.java
@@ -0,0 +1,11 @@
+package com.rocinante.directory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopDirectoryApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(ShopDirectoryApplication.class, args);
+  }
+}
