60d4ac025e2b147d1b61cac670760eb773df9010
Captain Rocinante <58204640+CaptainRocinante@users.noreply.github.com>
2021-03-31 09:53:10 -0700

    Async Crawling Controller

--- shops/src/main/java/com/rocinante/shops/controllers/CrawlingController.java
+++ shops/src/main/java/com/rocinante/shops/controllers/CrawlingController.java
@@ -1,24 +1,11 @@
 package com.rocinante.shops.controllers;
 
-import com.rocinante.crawlers.MapCrawlContext;
-import com.rocinante.crawlers.category.Category;
-import com.rocinante.crawlers.category.CategoryCrawler;
-import com.rocinante.crawlers.summary.SummaryCrawler;
-import com.rocinante.shops.api.CategoryDto;
 import com.rocinante.shops.api.MerchantDto;
-import com.rocinante.shops.datastore.dao.MerchantDao;
-import com.rocinante.shops.datastore.dao.MerchantInferredCategoryDao;
-import com.rocinante.shops.datastore.entities.Merchant;
-import com.rocinante.shops.datastore.entities.MerchantInferredCategory;
-import java.time.Instant;
-import java.time.ZoneOffset;
-import java.util.List;
-import java.util.Optional;
+import com.rocinante.shops.service.AsyncCrawlingService;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,43 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/crawl", produces = MediaType.APPLICATION_JSON_VALUE)
 @Slf4j
 public class CrawlingController {
-  private final MerchantDao merchantDao;
-  private final MerchantInferredCategoryDao merchantInferredCategoryDao;
-  private final CategoryCrawler categoryCrawler;
-  private final SummaryCrawler summaryCrawler;
+  private final AsyncCrawlingService asyncCrawlingService;
 
   @PostMapping("/categories")
-  @Async
   public void crawlCategories(@RequestBody MerchantDto merchantDto) {
-    log.info("Begin Category Crawl for merchant {}", merchantDto.getUuid());
-    final Merchant merchant = merchantDao
-        .findById(UUID.fromString(merchantDto.getUuid()))
-        .orElseThrow();
-    log.info("Merchant found {} with website {}", merchant.getName(), merchant.getUrl());
-    final List<Category> categories = categoryCrawler.crawlUrl(merchant.getUrl(),
-        new MapCrawlContext(null));
-
-    log.info("Category Count: {}", categories.size());
-    for (final Category category: categories) {
-      log.info("Handling {}", category.toString());
-
-      final Optional<MerchantInferredCategory> existingCategoryOpt = merchantInferredCategoryDao
-          .findByMerchantUuidAndUrl(merchant.getUuid(), category.getCategoryUrl());
-
-      if (existingCategoryOpt.isPresent()) {
-        final MerchantInferredCategory existingCategory = existingCategoryOpt.get();
-        log.info("Existing category found {}", existingCategory.getUuid());
-        existingCategory.setName(category.getCategoryName());
-        merchantInferredCategoryDao.save(existingCategory);
-      } else {
-        final MerchantInferredCategory newCategory =
-            new MerchantInferredCategory(merchant, category);
-        var result = merchantInferredCategoryDao.save(newCategory);
-        log.info("New category saved {}", result.getUuid());
-      }
-    }
-    merchant.setLastCrawledAt(Instant.now().atOffset(ZoneOffset.UTC));
-    merchantDao.save(merchant);
-    log.info("End Category Crawl for merchant {}", merchantDto.getUuid());
+    asyncCrawlingService.crawlAndSaveCategoriesForMerchant(UUID.fromString(merchantDto.getUuid()));
   }
 }
---
+++ shops/src/main/java/com/rocinante/shops/service/AsyncCrawlingService.java
@@ -0,0 +1,64 @@
+package com.rocinante.shops.service;
+
+import com.rocinante.crawlers.MapCrawlContext;
+import com.rocinante.crawlers.category.Category;
+import com.rocinante.crawlers.category.CategoryCrawler;
+import com.rocinante.crawlers.summary.SummaryCrawler;
+import com.rocinante.shops.datastore.dao.MerchantDao;
+import com.rocinante.shops.datastore.dao.MerchantInferredCategoryDao;
+import com.rocinante.shops.datastore.entities.Merchant;
+import com.rocinante.shops.datastore.entities.MerchantInferredCategory;
+import java.time.Instant;
+import java.time.ZoneOffset;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+@Slf4j
+public class AsyncCrawlingService {
+  private final MerchantDao merchantDao;
+  private final MerchantInferredCategoryDao merchantInferredCategoryDao;
+  private final CategoryCrawler categoryCrawler;
+  private final SummaryCrawler summaryCrawler;
+
+  @Async
+  public void crawlAndSaveCategoriesForMerchant(UUID merchantUuid) {
+    log.info("Begin Category Crawl for merchant {}", merchantUuid);
+    final Merchant merchant = merchantDao
+        .findById(merchantUuid)
+        .orElseThrow();
+    log.info("Merchant found {} with website {}", merchant.getName(), merchant.getUrl());
+    final List<Category> categories = categoryCrawler.crawlUrl(merchant.getUrl(),
+        new MapCrawlContext(null));
+
+    log.info("Category Count: {}", categories.size());
+    for (final Category category: categories) {
+      log.info("Handling {}", category.toString());
+
+      final Optional<MerchantInferredCategory> existingCategoryOpt = merchantInferredCategoryDao
+          .findByMerchantUuidAndUrl(merchant.getUuid(), category.getCategoryUrl());
+
+      if (existingCategoryOpt.isPresent()) {
+        final MerchantInferredCategory existingCategory = existingCategoryOpt.get();
+        log.info("Existing category found {}", existingCategory.getUuid());
+        existingCategory.setName(category.getCategoryName());
+        merchantInferredCategoryDao.save(existingCategory);
+      } else {
+        final MerchantInferredCategory newCategory =
+            new MerchantInferredCategory(merchant, category);
+        var result = merchantInferredCategoryDao.save(newCategory);
+        log.info("New category saved {}", result.getUuid());
+      }
+    }
+    merchant.setLastCrawledAt(Instant.now().atOffset(ZoneOffset.UTC));
+    merchantDao.save(merchant);
+    log.info("End Category Crawl for merchant {}", merchantUuid);
+  }
+
+}
