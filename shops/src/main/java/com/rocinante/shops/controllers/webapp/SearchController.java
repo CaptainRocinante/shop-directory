1277ff2295c5ba3d766e9811ddcb5ae3532ed43e
Captain Rocinante <58204640+CaptainRocinante@users.noreply.github.com>
2021-04-06 20:31:08 -0700

    Product grid ordering fix

--- shops/src/main/java/com/rocinante/shops/controllers/webapp/SearchController.java
+++ shops/src/main/java/com/rocinante/shops/controllers/webapp/SearchController.java
@@ -6,6 +6,7 @@ import com.rocinante.shops.service.sync.SearchService;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @AllArgsConstructor
+@Slf4j
 public class SearchController {
   private final SearchService searchService;
 
@@ -22,6 +24,7 @@ public class SearchController {
         searchService.search(query).stream()
             .map(Product::toProductDto)
             .collect(Collectors.toList());
+    productDtoList.forEach(p -> log.info(p.toString()));
 
     model.addAttribute("products", productDtoList);
     return "turboFragments/products";
