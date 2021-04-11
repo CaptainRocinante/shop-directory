e197f7969fb7c3f00ea6473f8f8abfbfe8d1ac1e
Captain Rocinante <58204640+CaptainRocinante@users.noreply.github.com>
2021-04-11 13:48:08 -0700

    Long brands list check

--- shops/src/main/java/com/rocinante/shops/service/sync/BnplService.java
+++ shops/src/main/java/com/rocinante/shops/service/sync/BnplService.java
@@ -2,7 +2,12 @@ package com.rocinante.shops.service.sync;
 
 import com.rocinante.shops.datastore.dao.BnplDao;
 import com.rocinante.shops.datastore.entities.BnplProvider;
+import com.rocinante.shops.datastore.entities.Merchant;
 import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,5 +20,14 @@ public class BnplService {
   @Transactional(readOnly = true)
   public List<BnplProvider> getAllBnplProviders() {
     return bnplDao.findAll();
+  }
+
+  @Transactional(readOnly = true)
+  public List<Merchant> getAllMerchantsForBnplProvider(UUID bnplUuid) {
+    return bnplDao.findById(bnplUuid).stream()
+        .map(BnplProvider::getMerchants)
+        .map(Set::stream)
+        .flatMap(Function.identity())
+        .collect(Collectors.toList());
   }
 }
