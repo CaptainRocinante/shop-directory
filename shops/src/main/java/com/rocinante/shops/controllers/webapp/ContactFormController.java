36d241d32eae20d91067ede2831dbf61424ae551
Captain Rocinante <58204640+CaptainRocinante@users.noreply.github.com>
2021-04-15 12:23:51 -0700

    Hookup Sendgrid

--- shops/src/main/java/com/rocinante/shops/controllers/webapp/ContactFormController.java
+++ shops/src/main/java/com/rocinante/shops/controllers/webapp/ContactFormController.java
@@ -1,6 +1,7 @@
 package com.rocinante.shops.controllers.webapp;
 
 import com.rocinante.shops.api.ContactForm;
+import com.rocinante.shops.service.sync.EmailService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -13,13 +14,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 @AllArgsConstructor
 @Slf4j
 public class ContactFormController {
+  private final EmailService emailService;
+
   @PostMapping(
       path = "/web/contact",
       consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE})
   public ResponseEntity<String> handleContactFormSubmission(ContactForm contactForm) {
     log.info("ContactForm {}", contactForm);
-    return new ResponseEntity<String>(
-        "Thank you for getting in touch. We'll contact you shortly.",
-        HttpStatus.OK);
+    try {
+      emailService.sendEmail(contactForm.getName() + " " + contactForm.getEmail(),
+          "admin@paylatergoods.com", contactForm.getSubject(),
+          "admin@paylatergoods.com", contactForm.getMessage());
+    } catch (Exception e) {
+      log.error("Failed to send email", e);
+    }
+
+    return new ResponseEntity<>(
+        "Thank you for getting in touch. We'll contact you shortly.", HttpStatus.OK);
   }
 }
---
+++ shops/src/main/java/com/rocinante/shops/service/sync/EmailService.java
@@ -0,0 +1,49 @@
+package com.rocinante.shops.service.sync;
+
+import com.sendgrid.Content;
+import com.sendgrid.Email;
+import com.sendgrid.Mail;
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class EmailService {
+  private final SendGrid sendGrid;
+
+  public EmailService(@Value("${sendgrid.api.key}") String sendgridApiKey) {
+    this.sendGrid = new SendGrid(sendgridApiKey);
+  }
+
+  public void sendEmail(String fromName, String fromEmail, String subjectStr, String toStr,
+      String contentStr) {
+    final Email from = new Email(fromEmail, fromName);
+    final Email to = new Email(toStr);
+    final Content content = new Content("text/plain", contentStr);
+    final Mail mail = new Mail(from, subjectStr, to, content);
+
+    final Request request = new Request();
+    try {
+      request.setMethod(Method.POST);
+      request.setEndpoint("mail/send");
+      request.setBody(mail.build());
+      final Response response = sendGrid.api(request);
+      if (response.getStatusCode() == HttpStatus.ACCEPTED.value()) {
+        log.info("Email sent successfully from {} to {} with subject {}",
+            fromEmail, toStr, subjectStr);
+      } else {
+        log.error("Email send failed from {} to {} with subject {}",
+            fromEmail, toStr, subjectStr);
+      }
+    } catch (IOException ex) {
+      throw new RuntimeException(ex);
+    }
+  }
+}
