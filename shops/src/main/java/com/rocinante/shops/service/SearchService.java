ed20386e51da5e4831f0f9a99a84806ff47eefec
Captain Rocinante <58204640+CaptainRocinante@users.noreply.github.com>
2021-04-19 13:57:51 -0700

    Search query improvement

--- shops/src/main/java/com/rocinante/shops/service/SearchService.java
+++ shops/src/main/java/com/rocinante/shops/service/SearchService.java
@@ -40,7 +40,7 @@ public class SearchService {
                       f.bool()
                           .must(f.match().field("enabled").matching(true))
                           .must(f.match().field("merchantEnabled").matching(true))
-                          .must(f.match().field("name").matching(query).boost(5.0f))
+                          .should(f.match().field("name").matching(query).boost(2.0f))
                           .should(f.match().field("merchantInferredCategoryText").matching(query))
                           .should(f.match().field("merchantNameText").matching(query))
                           .should(f.match().field("bnplNameText").matching(query));
@@ -83,7 +83,7 @@ public class SearchService {
                       f.bool()
                           .must(f.match().field("enabled").matching(true))
                           .must(f.match().field("merchantEnabled").matching(true))
-                          .must(f.match().field("name").matching(query).boost(5.0f))
+                          .should(f.match().field("name").matching(query).boost(2.0f))
                           .should(f.match().field("merchantInferredCategoryText").matching(query))
                           .should(f.match().field("merchantNameText").matching(query))
                           .should(f.match().field("bnplNameText").matching(query));
