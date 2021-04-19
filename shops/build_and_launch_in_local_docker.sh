d1feeeca17432851c39e64b0b60ee7be75fb8105
Captain Rocinante <58204640+CaptainRocinante@users.noreply.github.com>
2021-04-18 18:52:37 -0700

    Add scraper to boot as well

--- shops/build_and_launch_in_local_docker.sh
+++ shops/build_and_launch_in_local_docker.sh
@@ -2,4 +2,4 @@
 
 ./mvnw install -DskipTests
 docker build -t shopdirectory .
-docker run --net=host --env-file .env.local -p 8080:8080 shopdirectory shop/
+docker run --net=host --env-file .env.local -p 8080:8080 shopdirectory .
