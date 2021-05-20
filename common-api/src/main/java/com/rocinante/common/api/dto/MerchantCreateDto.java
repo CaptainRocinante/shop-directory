e7459ed20b2c727117762461814c325684c1ac0f
Captain Rocinante <58204640+CaptainRocinante@users.noreply.github.com>
2021-05-20 16:53:20 -0700

    Fixes

--- common-api/src/main/java/com/rocinante/common/api/dto/MerchantCreateDto.java
+++ common-api/src/main/java/com/rocinante/common/api/dto/MerchantCreateDto.java
@@ -1,10 +1,8 @@
 package com.rocinante.common.api.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class MerchantCreateDto {
   private final String bnplUuid;
   private final String merchantName;
